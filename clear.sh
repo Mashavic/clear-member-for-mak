206610920a408e85321cf9147d5494eb290c2e08
Elektra Norberto <elektrno@ge-d10.nsk.21-school.ru>
2023-08-12 10:42:07 +0700

    'add file clear.sh'

---
+++ clear.sh
@@ -0,0 +1,22 @@
+rm -rf ~/Library/Caches/*
+rm -rf ~/Library/42_cache
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
+rm -rf ~/Library/Application\ Code/Cache/*
+rm -rf ~/Library/Application\ Code/CachedData/*
+rm -rfv ~/Library/Caches/*
+rm -rfv ~/Library/Application\ Support/Slack/Cache/*
+rm -rfv ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+rm -rfv ~/Library/Group\ Containers/6N38VWS5BX.ru.keepcoder.Telegram/account-570841890615083515/postbox/*
+rm -rfv ~/Library/Caches
+rm -rfv ~/Library/Application\ Support/Code/Cache
+rm -rfv ~/Library/Application\ Support/Code/CachedData
+rm -rfv ~/Library/Application\ Support/Code/CachedExtension
+rm -rfv ~/Library/Application\ Support/Code/CachedExtensions
+rm -rfv ~/Library/Application\ Support/Code/CachedExtensionVSIXs
+rm -rfv ~/Library/Application\ Support/Code/Code\ Cache
+rm -rfv ~/Library/Application\ Support/Slack/Cache
+rm -rfv ~/Library/Application\ Support/Slack/Code\ Cache
+rm -rfv ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+rm -rfv ~/Library/Application\ Support/Code/User/workspaceStorage
